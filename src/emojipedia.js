f46e5b267bc58acf59ec24fd44ecabbbf8adecd2
REMCsin <cmer718@gmail.com>
2024-10-10 10:34:41 +0200

    Add files via upload

---
+++ src/emojipedia.js
@@ -0,0 +1,88 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym.",
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you.",
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“.",
+  },
+  {
+    id: 4,
+    emoji: "😎️",
+    name: "Face With Sunglasses",
+    meaning:
+      "I'm the king of the world! Smart and casual, this 😎 emoji features a face with a wide, closed smile and black sunglasses. It expresses coolness and confidence and is used to convey a carefree, relaxed attitude or to express that something is excellent. If you are confident in yourself, have done something particularly well and are proud of it, this emoji is an appropriate choice. It is also a symbol of sunshine, summer vacations, and beach vacations. It can be used in situations where you feel a sense of superiority or serenity. In some cases, the emoji even conveys a bit of irony or fun in your own coolness. All in all, it's versatile and helps convey positive emotions and attitudes in communication.",
+  },
+  {
+    id: 5,
+    emoji: "😈️",
+    name: "Smiling Face With Horns",
+    meaning:
+      "Mischievously grinning goblin smiley with small devil horns. Wants to cause trouble, is a real teaser or planning something ugly just now.",
+  },
+  {
+    id: 6,
+    emoji: "🤡️",
+    name: "Clown Face",
+    meaning:
+      "Stop fooling around! Made-up clown face from the circus world. Stands for fun and entertainment, but can also trigger fears in certain people - at least since Stephen King's horror movie “It“.",
+  },
+  {
+    id: 7,
+    emoji: "🫠️",
+    name: "Melting Face",
+    meaning:
+      "I'm melting! This smiling face becomes liquid on the underside. It either expresses that it's incredibly hot or that you're feeling uncomfortable right now: I'd like to sink into the ground!",
+  },
+  {
+    id: 8,
+    emoji: "😭️",
+    name: "Loudly Crying Face",
+    meaning:
+      "A face crying violently 😭 symbolizes despair and inconsolability, while the tears flow from the eyes like a waterfall. It represents feelings of sadness, pain, or defeat, and is sometimes used in ironic contexts. For example, when something is so funny that you can hardly stop yourself from laughing, or when someone is making a drama out of an insignificant situation. This emoji is one of the most popular on social media sites like Twitter and expresses some intense emotions like pride, overwhelming joy or even relief. The interpretation of this emoji varies depending on the context and can have both serious and humorous meanings.",
+  },
+  {
+    id: 9,
+    emoji: "🍑️",
+    name: "Peach",
+    meaning:
+      "Soft skin, juicy and sweet pulp, aromatic smell: the peach is considered a seductive fruit. Due to the characteristic shape, the most commonly used emoji for the female butt.",
+  },
+  {
+    id: 10,
+    emoji: "🍆️",
+    name: "Eggplant",
+    meaning:
+      "The eggplant is associated with the male sex. It is one of the sex symbols. Is hated and loved on Instagram #freetheeggplant.",
+  },
+  {
+    id: 11,
+    emoji: "🇪🇸",
+    name: "Flag España",
+    meaning:
+      "Es un flag sequence que combina 🇪 Regional Indicator Symbol Letter E and 🇸 Regional Indicator Symbol Letter S. Estos se muestran como un solo emoji en las plataformas compatibles.",
+  },
+  {
+    id: 12,
+    emoji: "🇩🇴",
+    name: "Flag República Dominicana",
+    meaning:
+      "Es un flag sequence que combina 🇩 Regional Indicator Symbol Letter D and 🇴 Regional Indicator Symbol Letter O. Estos se muestran como un solo emoji en las plataformas compatibles.",
+  },
+];
+
+export default emojipedia;
